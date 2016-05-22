f2a11ee016282894e461a95e9dd30668ce6b5297
Paulo Henrique de Siqueira <paulohenriqu@hotmail.com>
2016-05-22 15:21:12 -0300

    [PHS] - Melhoria dos parametros nas queries do searchExchangeableCardsController.js

--- app/controllers/searchExchangeableCardsController.js
+++ app/controllers/searchExchangeableCardsController.js
@@ -1,103 +1,93 @@
 
-angular.module('mytodoApp').controller('searchExchangeableCardController', 
+angular.module('mytodoApp').controller('searchExchangeableCardController',
+        function ($scope, $http, $state, Backand) {
 
-  function($scope, $http, $state, Backand) {
-           
-        var url = 'https://api.deckbrew.com/mtg';
-        var self = this;
-        $scope.colors = [];
-        $scope.cards = [];
-        $scope.sets = [];
-        $scope.types = [];
+            var url = 'https://api.deckbrew.com/mtg';
+            var self = this;
+            $scope.colors = [];
+            $scope.cards = [];
+
+            $scope.troca = function (id) {
+                $state.go('exchangeCards', {id: id});
+            }
+
+            /* 
+             Filter cards using the Backand custom query filtraCartasTroca
+             SELECT * FROM card WHERE exchangeable=1 AND IdUsers!='{{id}}' AND Colors like '%{{color}}%' AND NAME like '%{{name}}%'
+             */
+            $scope.searchCards = function () {
+
+                var cor = $scope.color;
+                var nome = $scope.name;
+
+                if (cor == undefined) {
+                    cor = "";
+                }
+                if (nome == undefined) {
+                    nome = "";
+                }
+
+                $http({
+                    method: 'GET',
+                    url: Backand.getApiUrl() + '/1/query/data/filtraCartasTroca',
+                    params: {
+                        parameters: {
+                            color: cor,
+                            id: localStorage.getItem('id'),
+                            name: nome
+                        }
+                    }
+                })
+                        .success(function (cont) {
+
+                            console.log(cont);
+                            $scope.cards = cont;
+
+                        })
+                        .error(function (erro) {
+                            console.log(erro);
+                        });
+            }
+
+            /*
+             Get all exchangeable cards using the Backand custom query todasCartasTroca
+             SELECT * FROM card WHERE exchangeable=1 AND idUsers!='{{id}}'
+             */
+
+            $scope.all = function () {
+
+                $http({
+                    method: 'GET',
+                    url: Backand.getApiUrl() + '/1/query/data/todasCartasTroca',
+                    params: {
+                        parameters: {
+                            id: localStorage.getItem('id')
+                        }
+                    }
+                })
+                        .success(function (cont) {
+                            $scope.cards = cont;
+
+                        })
+                        .error(function (erro) {
+                            console.log(erro);
+                        });
+            }
 
         
-		$scope.troca=function(id){
-			$state.go('exchangeCards', {id: id});
-		}
-		
-		$scope.searchCards=function(){
-		//var filtro="Colors like '%%' AND Name like '%%'";
-		
-		var cor=$scope.color;
-		var nome=$scope.name;
-		
-		if(cor==undefined){
-			cor="";
-		}
-		if(nome==undefined){
-			nome="";
-		}
-		
-		console.log("Cor: "+cor);
-		console.log("Nome: "+nome);
-		
-		 $http({
-            method: 'GET',
-            url: Backand.getApiUrl() + "/1/query/data/filtraCartasTroca?parameters=%7B%22id%22:%22"+localStorage.getItem('id')+"%22,%22name%22:%22"+nome+"%22,%22color%22:%22"+cor+"%22%7D"})
-                .success(function (cont) {
-				
-				console.log(cont);
-                    $scope.cards=cont;                   
+            // return all colors of mana
+            $scope.colorsAll = function () {
+                $http.get(url + '/colors')
+                        .then(function successCallback(response) {
+                            $scope.colors = response.data;
+                        }, function errorCallback(response) {
+                            alert("Errmac");
+                        });
+            };
 
-                })
-                .error(function (erro) {
-                    console.log(erro);
-                });
-		
-		
-		}
-		
-		$scope.all=function(){		
-		
-		  $http({
-            method: 'GET',
-            url: Backand.getApiUrl() + '/1/query/data/todasCartasTroca?parameters=%7B%22id%22:%22'+localStorage.getItem('id')+'%22%7D'})
-                .success(function (cont) {
-                    $scope.cards=cont;                   
+     
+           
+            $scope.colorsAll();
+            $scope.all();
 
-                })
-                .error(function (erro) {
-                    console.log(erro);
-                });
-		
-		
-		}
-
-        // return all sets of card
-        $scope.setsAll = function(){
-          $http.get(url + '/sets')
-            .then(function successCallback(response) {
-              $scope.sets = response.data;
-            }, function errorCallback(response) {
-              alert("Errmac");
-            });
-        };
-
-        // return all colors of mana
-        $scope.colorsAll = function(){
-          $http.get(url + '/colors')
-            .then(function successCallback(response) {
-              $scope.colors = response.data;
-            }, function errorCallback(response) {
-              alert("Errmac");
-            });
-        };
-
-        // Get all types of cards
-        $scope.typesAll = function(){
-          $http.get(url + '/types')
-            .then(function successCallback(response) {
-              $scope.types = response.data;
-            }, function errorCallback(response) {
-              alert("Errmac");
-            });
-        };
-
-        $scope.cardDetails = function(id) {
-          $state.go('cardDetails', {multiverseid: id});
-        };
-		
-		$scope.colorsAll();
-		$scope.all();
-		
         });
